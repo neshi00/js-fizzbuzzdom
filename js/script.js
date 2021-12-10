6f69b0c7b5e3feaa0410b4a56deda31a75d03c4d
neshi00 <luca.ascione85@gmail.com>
2021-12-10 14:42:05 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -24,8 +24,28 @@ for (let i = 1; i <= 50; i++) {
     }
 }
 
-/* 
-MILESTONE 2
-Dato un container nel DOM, 
-appendi un elemento html con il numero o la stringa corretta.
-*/
+
+// MILESTONE 2
+
+// mi aggancio alla classe HTML container-boxes
+const containerBoxes = document.querySelector('.container-boxes');
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0 ) {
+        // a condizione verificata, eseguo comando => FizzBuzz
+        // nel container HTML identificato in precedenza => .container-boxes
+        containerBoxes.innerHTML += `<span>${'FizzBuzz'} ${'-'}</span> `;
+    }
+    else if (i % 5 === 0 ) {
+        containerBoxes.innerHTML += `<span>${'Buzz'} ${'-'}</span> `;
+    }
+    else if (i % 3 === 0 ) {
+        containerBoxes.innerHTML += `<span>${'Fizz'} ${'-'}</span> `;
+    }
+    else {
+        containerBoxes.innerHTML += `<span>${i} ${'-'}</span> `
+    }
+}
+
+
+
